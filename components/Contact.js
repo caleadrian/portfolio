1a95291e925e6a6428e8f65fdb05965f2576b108
kirito27 <keyladrian7@gmail.com>
2022-08-23 08:32:30 +0800

    Added netlify email functionlity

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image';
-import avatar from '../resources/image/avatar_call_me.jpg'
+import avatar from '../resources/image/avatar_call_me.jpg';
+import Script from 'next/script';
 
 function Contact() {
     return (
         <div id='contact' className='snap-start pt-5 sm:pt-36 text-center px-0 sm:px-0  sm:h-screen'>
-
+            <Script src="https://www.google-analytics.com/analytics.js" />
             <div className='text-center'>
                 <div className='text-xs font-medium text-gray-500 mb-1'>My tech powers</div>
                 <div className='text-[#4db5ff] text-2xl'>Experience & <span className='text-orange-400'>Skills</span></div>
@@ -21,7 +22,41 @@ function Contact() {
                 </div>
 
                 <div className='bg-white bg-opacity-5 w-7/12 rounded-md'>
-                    form
+                    <form
+                        name='contact'
+                        method='post'
+                        data-netlify='true'
+                        onSubmit='submit'
+                        data-netlify-honeypot='bot-field'
+                        action='/#contact'>
+
+                        <input type='hidden' name='form-name' value='contact' />
+
+                        <div hidden>
+                            <input name='bot-field' />
+                        </div>
+
+                        <div className='flex flex-col gap-y-2'>
+                            <div>
+                                <label>Name</label>
+                                <input className='text-black' type='text' name='name' required />
+                            </div>
+
+                            <div>
+                                <label>Email</label>
+                                <input className='text-black' type='email' name='name' required />
+                            </div>
+
+                            <div>
+                                <label>Message</label>
+                                <textarea className='text-black' name='message' required></textarea>
+                            </div>
+
+                            <button type='submit'>Submit</button>
+                        </div>
+
+
+                    </form>
                 </div>
             </div>
 
