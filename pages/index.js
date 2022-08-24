227e8c047bfbf8a6aa9f40c1e0c3b01d525e61f7
kirito27 <keyladrian7@gmail.com>
2022-08-24 16:04:41 +0800

    Fix icons size & Home Section Margin

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,18 @@ export default function Home() {
 
   return (
     <div className="flex flex-col py-28 px-10 sm:px-0 gap-56">
-      <div id="home" className=" text-center h-screen">
+      <div id="home" className=" text-center h-screen mt-32">
         <div className="text-sm font-medium mb-2">Hello I&apos;m</div>
         <div className="text-6xl font-thin mb-1">Cale Adrian Lacson</div>
         <div className="text-[#4db5ff] font-medium">Software Engineer</div>
 
         <div className="mt-3">
           <a href="https://www.linkedin.com/in/cale-adrian/" target="_blank" rel="noreferrer" className="font-semibold inline-flex items-center rounded mr-3">
-            <BsLinkedin className="text-[#4db5ff]" />
+            <BsLinkedin className="text-[#4db5ff] h-5 w-5" />
           </a>
 
           <a href="https://github.com/caleadrian/" target="_blank" rel="noreferrer" className="font-semibold inline-flex items-center rounded">
-            <FaGithub className="text-[#4db5ff]" />
+            <FaGithub className="text-[#4db5ff] h-5 w-5" />
           </a>
         </div>
 
